0af5eea7024d35f8aca93c1090ea5a14c599a4dd
SobakaBekka <st69704@upce.cz>
2024-08-26 15:20:54 +0200

    done

--- src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
+++ src/main/java/com/epam/mjc/collections/combined/LessonsGetter.java
@@ -1,10 +1,12 @@
 package com.epam.mjc.collections.combined;
-
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class LessonsGetter {
     public Set<String> getLessons(Map<String, List<String>> timetable) {
+        Set<String> lessons = new HashSet<>();
+        for (List<String> dayLessons : timetable.values()) {
+            lessons.addAll(dayLessons);
+        }
+        return lessons;
     }
 }
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,16 @@
 package com.epam.mjc.collections.combined;
-
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer, Set<String>> resultMap = new HashMap<>();
+
+        for (String key : sourceMap.keySet()) {
+            int length = key.length();
+            resultMap.putIfAbsent(length, new HashSet<>());
+            resultMap.get(length).add(key);
+        }
+
+        return resultMap;
     }
 }
